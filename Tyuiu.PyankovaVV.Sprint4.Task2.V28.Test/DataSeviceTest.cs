5ba7d55953306ea8d952432356d031c4f21eb59c
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-11-05 15:04:06 +0500

    Task2

---
+++ Tyuiu.PyankovaVV.Sprint4.Task2.V28.Test/DataSeviceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PyankovaVV.Sprint4.Task2.V28.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint4.Task2.V28.Test
+{
+    [TestClass]
+    public class DataSeviceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+        int[] array = { 7, 6, 3, 8, 5, 7, 6, 7, 6, 5, 4, 8 };
+        int res = ds.Calculate(array);
+        int wait = 34;
+        Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PyankovaVV.Sprint4.Task2.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyankovaVV.Sprint4.Task2.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyankovaVV.Sprint4.Task2.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97f1376b-382d-4b2f-8c3d-72dff3b12ca6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
